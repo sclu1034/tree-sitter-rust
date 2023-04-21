5dcc1e3df8cdce5365b65dac4692e350a2c396e5
Lucas Schwiderski <lucas@lschwiderski.de>
2023-04-21 21:34:08 +0200

    Add macro language injections
    Various crates provide macros that parse foreign languags like HTML,
    JSON or Lua.

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,3 +1,47 @@
+; Macros from crates that parse syntax that we can inject, like Yew's `html!`
+((macro_invocation
+  macro: (identifier) @_name
+  (token_tree) @injection.content)
+ (#match? @_name "^html$")
+ (#set! injection.language "html")
+ (#set! injection.include-children))
+
+((macro_invocation
+  macro: (identifier) @_name
+  (token_tree) @injection.content)
+ (#match? @_name "^python$")
+ (#set! injection.language "python")
+ (#set! injection.include-children))
+
+((macro_invocation
+  macro: (identifier) @_name
+  (token_tree) @injection.content)
+ (#match? @_name "^json$")
+ (#set! injection.language "json")
+ (#set! injection.include-children))
+
+((macro_invocation
+  macro: (identifier) @_name
+  (token_tree) @injection.content)
+ (#match? @_name "^toml$")
+ (#set! injection.language "toml")
+ (#set! injection.include-children))
+
+((macro_invocation
+  macro: (identifier) @_name
+  (token_tree) @injection.content)
+ (#match? @_name "^lua|compile_inline_teal$")
+ (#set! injection.language "lua")
+ (#set! injection.include-children))
+
+; `chunk!` is rather generic, so we only match the full path
+((macro_invocation
+  macro: (scoped_identifier) @_name
+  (token_tree) @injection.content)
+ (#match? @_name "^mlua::chunk|lua|compile_inline_teal$")
+ (#set! injection.language "lua")
+ (#set! injection.include-children))
+
 ((macro_invocation
   (token_tree) @injection.content)
  (#set! injection.language "rust")
